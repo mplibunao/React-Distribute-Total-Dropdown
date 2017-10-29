eaf1630821c2be8ce51064d39be0d5c2270ceba2
mplibunao <markpaololibunao@gmail.com>
2017-10-29 09:39:57 +0800

    completed initial setup of props without the callback function props

--- src/DistributeTotalDropdown/Dropdown.js
+++ src/DistributeTotalDropdown/Dropdown.js
@@ -2,7 +2,28 @@ import React from "react";
 import { FormControl, FormGroup } from "react-bootstrap";
 
 class Dropdown extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+    }
 
+    render() {
+        const { available, breakdown, name } = this.props;
+        const availableArray = Array(available).fill(null);
+        
+        const dropdown = availableArray.map(option => {
+            return (
+                <FormGroup className="col-md-6">
+                    <FormControl componentClass="select">
+                        <option>0</option>
+                    </FormControl>
+                </FormGroup>
+            );
+        });
+            
+        return (
+            {dropdown}
+        );
     }
 }
+
+export default Dropdown;
--- src/DistributeTotalDropdown/index.js
+++ src/DistributeTotalDropdown/index.js
@@ -1,8 +1,26 @@
 import React from "react";
+import Dropdown from "./Dropdown";
 
 class DistributeTotalDropdown extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+    }
 
+    render() {
+        const { breakdown, available, names } = this.props;
+        const dropdowns = names.map(name => {
+            <Dropdown
+                name={name}
+                breakdown={breakdown[name]}
+                available={available[name]}
+            />
+        });
+
+        return (
+            <div>
+                {dropdowns}
+            </div>
+        );
     }
 }
 
