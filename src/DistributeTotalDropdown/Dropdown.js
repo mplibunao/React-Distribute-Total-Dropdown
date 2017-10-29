41287592e8b951ffd790240ff4ba8b9b6f4655fd
mplibunao <markpaololibunao@gmail.com>
2017-10-29 19:53:39 +0800

    refactored dropdown component into a functional component

--- src/DistributeTotalDropdown/Dropdown.js
+++ src/DistributeTotalDropdown/Dropdown.js
@@ -1,29 +1,23 @@
 import React from "react";
 import { FormControl, FormGroup } from "react-bootstrap";
 
-class Dropdown extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const { available, breakdown, name, onChange } = this.props;
-        const availableArray = Array(available + 1).fill(null);
-        
-        const options = availableArray.map((option, i) => {
-            return (
-                <option>{i}</option>
-            );
-        });
-            
+const Dropdown = (props) => {
+    const { available, breakdown, name, onChange } = props;
+    const availableArray = Array(available + 1).fill(null);
+    
+    const options = availableArray.map((option, i) => {
         return (
-            <FormGroup className="col-md-6">
-                <FormControl componentClass="select" value={breakdown} onChange={(event) => onChange(event, name)}>
-                    {options}
-                </FormControl>
-            </FormGroup>
+            <option>{i}</option>
         );
-    }
+    });
+            
+    return (
+        <FormGroup className="col-md-6">
+            <FormControl componentClass="select" value={breakdown} onChange={(event) => onChange(event, name)}>
+                {options}
+            </FormControl>
+        </FormGroup>
+    );
 }
 
 export default Dropdown;
